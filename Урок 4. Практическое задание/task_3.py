fceb58f76076df89eb9b151acd5986b416cbb4d2
sergey-vol <sergey.volnukhin@timebook.ru>
2020-12-30 11:55:28 +0300

    hw_lesson_4

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -9,19 +9,16 @@
 
 Сделайте вывод, какая из трех реализаций эффективнее и почему
 """
-import sys
-import cProfile
-from timeit import timeit
 
 
 def revers(enter_num, revers_num=0):
     if enter_num == 0:
-        return revers_num
+        return
     else:
         num = enter_num % 10
         revers_num = (revers_num + num / 10) * 10
         enter_num //= 10
-        return revers(enter_num, revers_num)
+        revers(enter_num, revers_num)
 
 
 def revers_2(enter_num, revers_num=0):
@@ -37,23 +34,3 @@ def revers_3(enter_num):
     revers_num = enter_num[::-1]
     return revers_num
 
-
-def main():
-    revers(num)
-    revers_2(num)
-    revers_3(num)
-
-
-sys.setrecursionlimit(10000)
-num = 5464565464565464564456 ** 100
-print(timeit('revers(num)', 'from __main__ import revers, num', number=100))
-print(timeit('revers_2(num)', 'from __main__ import revers_2, num', number=100))
-print(timeit('revers_3(num)', 'from __main__ import revers_3, num', number=100))
-cProfile.run('main()')
-
-"""
-Реализация №3 revers_3 самая эфективная и быстрая, так при любом кол-ве цифр, она выполняет одинаковое кол-во шагов
-Но не всегда срез выигрывает, например поиск палиндрома, лучше использовать цикл.
-Но для задач подобного тпа, реалдизация со срезом, самая быстарая по всем параметрам.
-
-"""
